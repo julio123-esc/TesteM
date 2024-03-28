e2ca79ca75f357815afb20469f53cc94182c7c61
julio123-esc <113455319+julio123-esc@users.noreply.github.com>
2024-03-28 13:26:33 -0300

    28/03

---
+++ scipit.js
@@ -0,0 +1,7 @@
+document.getElementById("botaoAmor").addEventListener("click", function() {
+      document.getElementById("mensagemAmor").style.display = "block";
+        document.getElementById("mensagemAmor").textContent = "Mel, eu te amo!";
+          // Tocar música de amor
+            document.getElementById("musicaAmor").play();
+            });
+})
